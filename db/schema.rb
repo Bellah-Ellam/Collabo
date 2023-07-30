c919ebd91ecc7313c79a2a5215d1b6cfd6a9e0a9
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-07-30 16:06:02 +0300

    merged sharons

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_07_27_211905) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_064204) do
->>>>>>> origin/dennis
   create_table "categories", force: :cascade do |t|
     t.string "category_name"
     t.integer "content_id", null: false
@@ -131,12 +124,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_064204) do
   end
 
   create_table "users", force: :cascade do |t|
-<<<<<<< HEAD
-    t.string "user_name"
-=======
     t.string "username"
     t.string "name"
->>>>>>> origin/dennis
     t.string "photo"
     t.date "doB"
     t.string "role"
@@ -148,7 +137,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_064204) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
