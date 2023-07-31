828c0df6069a0cb9df9c6f5611adfbab83c78108
Dennis K. Cherogony <cherogonydennis9.com>
2023-07-31 13:21:04 +0300

    Updated Frontend

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-    # has_many :bookings, dependent: :destroy
-    # has_many :properties, through: :bookings
-    has_secure_password
-
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
-
-end
