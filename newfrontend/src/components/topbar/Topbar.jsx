3a612bb2ce4406a4c39114d71274453da25422f3
Dennis K. Cherogony <cherogonydennis9.com>
2023-08-03 17:10:21 +0300

    Trying to Sort Navbar

--- newfrontend/src/components/topbar/Topbar.jsx
+++ newfrontend/src/components/topbar/Topbar.jsx
@@ -6,8 +6,10 @@ import { Link } from "react-router-dom";
 
 
 export default function Topbar(user) {
-  const { token } = useContext(AuthContext);
+  const { currentUser, logout, token } = useContext(AuthContext);
   const [notificationsCount, setNotificationsCount] = useState(0);
+
+  console.log("New Staff", currentUser)
 
   // Fetch notifications on component mount
   useEffect(() => {
@@ -36,9 +38,6 @@ export default function Topbar(user) {
     }
   };
 
-
-
-
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -55,23 +54,32 @@ export default function Topbar(user) {
       </div>
       <div className="topbarRight">
         <div className="topbarLinks">
-          <Link to="/" className="topbarLink">Home </Link>
-          {!token && (
-            <>
-              <Link to="/login" className="topbarLink">Login </Link>
-              <Link to="/register" className="topbarLink">Register </Link>
-              <Link to="/profile" className="topbarLink">Profile</Link>
-            </>
-          )}
+          <Link to="/" className="topbarLink">Home </Link> 
+          <Link to="/login" className="topbarLink">Login </Link>
+          <Link to="/register" className="topbarLink">Register </Link>
+          
+          
         </div>
+
         <div className="topbarIcons">
           <div className="topbarIconItem">
             <Notifications />  {notificationsCount > 0 && <span className="topbarIconBadge">{notificationsCount}</span>}
           </div>
         </div>
-        <Link to={`/profile/${user.username}`} className="topbarImgLink">
-          <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
-        </Link>
+
+
+              <li className="nav-item dropdown">
+                  {/* <Link to="/profile" className="topbarLink">Profile</Link> */}
+                  <Link to="/profile" className="topbarImgLink">
+                    <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                  </Link>
+
+                <ul className="dropdown-menu">
+                      <li><Link to="/profile" className="dropdown-item text-white" href="#">{currentUser && currentUser.email}</Link></li>
+                      <li><a className="dropdown-item text-white" href='#' onClick={() => logout()}>logout</a></li>
+                </ul>
+              </li>
+
       </div>
     </div>
   );
