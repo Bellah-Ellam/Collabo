40f24063e28fb966e10b8a3d2b9623596cc8d26c
nicholas <nickkiim7@gmail.com>
2023-08-04 10:46:26 +0300

    af

---
+++ newfrontend/src/App.js
@@ -0,0 +1,31 @@
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
+import Register from "./pages/register/register";
+// import Topbar from "./components/topbar/Topbar";
+// import Sidebar from "./components/sidebar/Sidebar";
+// import Feed from "./components/feed/Feed";
+// import Rightbar from "./components/rightbar/Rightbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+        <BrowserRouter>
+  
+            
+                <Routes>
+                
+                    <Route index element={<Home />}/>
+                    {/* <Route path="/Home" element={<Home />}/> */}
+                    <Route path="/login" element={<Login />}/>
+                    <Route path="/register" element={<Register />}/>
+                    <Route path="/profile" element={<Profile />}/>
+                
+                </Routes>
+
+
+        </BrowserRouter>
+  )
+}
+
+export default App;
--- newfrontend/src/components/feed/Feed.jsx
+++ newfrontend/src/components/feed/Feed.jsx
@@ -1,33 +1,15 @@
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
-import { useContext, useEffect, useState } from "react";
-import { AuthContext } from "../../Context/AuthContext";
+import { Posts } from "../../dummyData";
 
 export default function Feed() {
-  const { currentUser } = useContext(AuthContext);
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch('api/v1/posts'); 
-        const data = await response.json();
-        setPosts(data);
-      } catch (error) {
-        console.error("Error fetching content:", error);
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
   return (
     <div className="feed">
       <div className="feedWrapper">
         <Share />
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
+        {Posts.map((p) => (
+          <Post key={p.id} post={p} />
         ))}
       </div>
     </div>
