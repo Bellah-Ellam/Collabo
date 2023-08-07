276dc731e9241d939e961c83908e30ca3c582817
Reagan <rokhanga@gmail.com>
2023-08-07 18:18:17 +0300

    modify seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,48 +24,48 @@ user1 = User.create!(
     coverPicture: "https://media.istockphoto.com/id/1426460860/photo/smiling-young-african-woman-sitting-with-laptop-in-cafe.webp?b=1&s=170667a&w=0&k=20&c=1Fj4bBhlOxBKN9zvuT8Ysli5MyBasNQW2KnYApO-r4c="
   )
   user2 = User.create!(
-    username: Faker::Internet.unique.user_name,
-    email: Faker::Internet.unique.email,
+    username: "mose",
+    email: "mose@gmai.com",
     password: "password",
     date_of_birth: Faker::Date.between(from: '1980-01-01', to: '2002-12-31'),
     editor: false, 
     admin: false,
-    profilePicture: "",
-    coverPicture: ""
+    profilePicture: "https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    coverPicture: "https://images.unsplash.com/photo-1615412704911-55d589229864?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
   )
   user3 = User.create!(
     username: Faker::Internet.unique.user_name,
-    email: Faker::Internet.unique.email,
+    email: "faker@gmail.com",
     password: "password",
     date_of_birth: Faker::Date.between(from: '1980-01-01', to: '2002-12-31'),
     editor: false, 
     admin: false,
-    profilePicture: "",
-    coverPicture: ""
+    profilePicture: "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    coverPicture: "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"
   )
   user4 = User.create!(
-    username: Faker::Internet.unique.user_name,
-    email: Faker::Internet.unique.email,
+    username: "dante",
+    email: "dante@gmail.com",
     password: "password",
     date_of_birth: Faker::Date.between(from: '1980-01-01', to: '2002-12-31'),
     editor: false, 
     admin: false,
-    profilePicture: "",
-    coverPicture: ""
+    profilePicture: "https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    coverPicture: "https://images.unsplash.com/photo-1614531341773-3bff8b7cb3fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
   )
   user5 = User.create!(
-    username: Faker::Internet.unique.user_name,
-    email: Faker::Internet.unique.email,
+    username: "sasha",
+    email: "sasha@gmail.com",
     password: "password",
     date_of_birth: Faker::Date.between(from: '1980-01-01', to: '2002-12-31'),
     editor: false, 
     admin: false,
-    profilePicture: "",
-    coverPicture: ""
+    profilePicture: "https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+    coverPicture: "https://images.unsplash.com/photo-1644325781920-a41ee5b914b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDM5fHhIeFlUTUhMZ09jfHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
   )
 
 # Create some posts
-  post1 = Post.create!(
+  post1 = user1.posts.create!(
     desc: "Great weekend my wonderful people",
     img: "https://images.unsplash.com/photo-1592861956120-e524fc739696?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVvcGxlJTIwZWF0aW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
     likes: [],
